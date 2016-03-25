58db63a497116648116338b45e4507c4db49a73c
Fabian Jakub <fabian@jakub.com>
2016-03-25 15:29:39 +0100

    fix country template

--- miroutes/urls.py
+++ miroutes/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from miroutes import views
 
 urlpatterns = [ url(r'^$', views.index, name='miroutes_index'),
-url(r'^(?P<country_id>\d+)/$', views.country_detail, name='country_detail'),
+url(r'^country(?P<country_id>\d+)/$', views.country_detail, name='country_detail'),
 
 url(r'^area(?P<area_id>\d+)/$', views.area_detail, name='area_detail'),
 
@@ -12,7 +12,7 @@ url(r'^spot(?P<spot_id>\d+)/$', views.spot_detail, name='spot_detail'),
 url(r'^spot(?P<spot_id>\d+)/edit$', views.spot_edit, name='spot_edit'),
 
 url(r'^wall(?P<wall_id>\d+)/$', views.wall_detail, name='wall_detail'),
-url(r'^(?P<spot_id>\d+)/add_wall$', views.add_wall, name='wall_add'),
+url(r'^spot(?P<spot_id>\d+)/add_wall$', views.add_wall, name='wall_add'),
 url(r'^wall(?P<wall_id>\d+)/edit$', views.wall_edit, name='wall_edit'),
 
 url(r'^route(?P<route_id>\d+)$', views.route_detail, name='route_detail'),
