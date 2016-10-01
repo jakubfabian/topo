5b9159648191d26a19168e9508b09fc617c46042
Jonas Hahn <jonas.hahn@datenhahn.de>
2016-10-01 17:31:57 +0200

    remove testapp

--- testapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.conf.urls import *
-
-hello = lambda request: HttpResponse("Hello .. mehr Power!")
-
-urlpatterns = [
-    url(r'^$', hello, name='hipower'),
-    ]
