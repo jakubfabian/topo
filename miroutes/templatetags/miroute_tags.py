d8a86fa04610637ff4c374ec5e0125e1b10e6f0e
Alois Dirnaichner <alois.dirnaichner@physik.uni-regensburg.de>
2016-10-13 17:08:48 +0200

    Catching exceptions in the linestring converstion. Unsuccessful conversion returns None now.

--- miroutes/templatetags/miroute_tags.py
+++ miroutes/templatetags/miroute_tags.py
@@ -1,17 +1,22 @@
 from django import template
 from ast import literal_eval
+import logging
 
 register = template.Library()
 
 @register.filter(name='linestringgeomfilter')
 def get_linestringgeom(value):
-    assert type(value.geom) == unicode, 'Not a valid unicode linestring %s' % str(value.geom)
 
-    geomdict = literal_eval(value.geom)
-    assert type(geomdict) == dict, 'Can not convert %s to a dict.' % str(geomdict)
+    try:
+        assert type(value.geom) == unicode, 'Not a valid unicode linestring %s' % str(value.geom)
+        geomdict = literal_eval(value.geom)
+        assert type(geomdict) == dict, 'Can not convert %s to a dict.' % str(geomdict)
 
-    # we reverse [x=lng, y=lat] to fit to [lat, lng]
-    return [[coord[1], coord[0]] for coord in geomdict['coordinates']]
+        # we reverse [x=lng, y=lat] to fit to [lat, lng]
+        return [[coord[1], coord[0]] for coord in geomdict['coordinates']]
+    except (AssertionError, KeyError) as err:
+        logging.log('Linestring %s could not be filtered: %s', value.geom, err)
+        return None
 
         
         
