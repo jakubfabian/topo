a372c56b5166851b7ce7da995025265c8aa26b94
Alois Dirnaichner <alois.dirnaichner@physik.uni-regensburg.de>
2016-10-13 17:17:21 +0200

    Undo the last commit after discussions.

--- miroutes/templatetags/miroute_tags.py
+++ miroutes/templatetags/miroute_tags.py
@@ -1,23 +1,19 @@
 from django import template
 from ast import literal_eval
-import logging
 
 register = template.Library()
 
 @register.filter(name='linestringgeomfilter')
 def get_linestringgeom(value):
+    """Filter GEOJson linestrings to generate a coordinate array which can be
+    used to make polylines.
 
-    try:
-        assert type(value.geom) == unicode, 'Not a valid unicode linestring %s' % str(value.geom)
-        geomdict = literal_eval(value.geom)
-        assert type(geomdict) == dict, 'Can not convert %s to a dict.' % str(geomdict)
+    Throws:
+        AssertionError, KeyError
+    """
+    assert type(value.geom) == unicode, 'Not a valid unicode linestring %s' % str(value.geom)
+    geomdict = literal_eval(value.geom)
+    assert type(geomdict) == dict, 'Can not convert %s to a dict.' % str(geomdict)
 
-        # we reverse [x=lng, y=lat] to fit to [lat, lng]
-        return [[coord[1], coord[0]] for coord in geomdict['coordinates']]
-    except (AssertionError, KeyError) as err:
-        logging.log('Linestring %s could not be filtered: %s', value.geom, err)
-        return None
-
-        
-        
-        
+    # we reverse [x=lng, y=lat] to fit to [lat, lng]
+    return [[coord[1], coord[0]] for coord in geomdict['coordinates']]        
